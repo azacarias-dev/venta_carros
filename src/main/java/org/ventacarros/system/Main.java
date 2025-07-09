f9067f809d1d9c580a4d8315981a4f3957c3d119
azacarias-dev <azacarias-2024007@kinal.edu.gt>
2025-07-09 13:51:30 -0600

    Se modifico el arcihvo main para que la primera vista a cargar sea InicioView.fxml

--- src/main/java/org/ventacarros/system/Main.java
+++ src/main/java/org/ventacarros/system/Main.java
@@ -32,7 +32,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         this.escenarioPrincipal = stage;
-        Productos();
+        Inicio();
         escenarioPrincipal.setTitle("Inicio");
         escenarioPrincipal.show();
     }
