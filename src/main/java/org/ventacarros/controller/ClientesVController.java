5b482625dc789cfa3e5f4945ab60e0968f1bf808
azacarias-dev <azacarias-2024007@kinal.edu.gt>
2025-07-11 19:02:01 -0600

    Se añadio el controller de ClientesView

---
+++ src/main/java/org/ventacarros/controller/ClientesVController.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package org.ventacarros.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+import org.ventacarros.system.Main;
+
+/**
+ * FXML Controller class
+ *
+ * @author jgome
+ */
+public class ClientesVController implements Initializable {
+
+    private Main principal;
+
+    public void setPrincipal(Main principal) {
+        this.principal = principal;
+    }
+    
+    
+    
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
